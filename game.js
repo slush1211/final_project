df432c13a185ad8bf5d8d3c5eb229185b39476af
slush1211 <adam@Adams-MacBook-Air.local>
2018-08-17 22:48:32 -0500

    added background tile

--- game.js
+++ game.js
@@ -13,15 +13,17 @@ var game = new Phaser.Game(config, document.getElementById("game"));
 
 function preload ()
 {
-
+    this.load.image('ground', 'assets/ground.png');
 }
 
 function create ()
 {
+    // Create a tilesprite (x, y, width, height, key)
+    this.add.tileSprite(0,0,396,529,'ground')
 
 }
 
 function update ()
 {
-    
+
 }
